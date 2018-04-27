d63337cc15375c4059d50b2a11dad30ebd5037b6
USER_NAME <EMAIL_ADDRESS>
2018-04-27 15:05:08 +0000

    user pages

--- config/app.php
+++ config/app.php
@@ -148,6 +148,7 @@ return [
         Illuminate\View\ViewServiceProvider::class,
         
         Collective\Html\HtmlServiceProvider::class,
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -214,6 +215,7 @@ return [
         
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Gravatar' => Thomaswelton\LaravelGravatar\Facades\Gravatar::class,
 
     ],
 

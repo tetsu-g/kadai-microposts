8efc00eee099865d7daf6d28d0f0fe3fe018228f
USER_NAME <EMAIL_ADDRESS>
2018-04-29 02:59:03 +0000

    post

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+Route::get('/', 'MicropostsController@index');
+
+
 Route::group(['middleware' => ['auth']], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
 
